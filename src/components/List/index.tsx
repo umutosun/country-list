dab82b43b2e8d94d1698e22061ba2ad100e7641e
Umut Tosun <umut35137@gmail.com>
2023-09-12 02:22:56 +0300

    Feat: checkbox

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -13,6 +13,9 @@ const List = ({ data, columns, setRowSelection }: ListProps) => {
 
   const [filtering, setFiltering] = useState<string>();
 
+  const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
+  const [selectedRowId, setSelectedRowId] = useState<string | null>(null);
+
   const { getHeaderGroups, getRowModel } = useReactTable({
     data: memoizedData,
     columns: memoizedColumns,
@@ -24,19 +27,37 @@ const List = ({ data, columns, setRowSelection }: ListProps) => {
     },
     onGlobalFilterChange: setFiltering,
   });
+
+  const handleCheckboxChange = (countryName: string, rowId: string) => {
+    if (selectedCountry === countryName) {
+      setSelectedCountry(null);
+      setSelectedRowId(null);
+    } else {
+      setSelectedCountry(countryName);
+      setSelectedRowId(rowId);
+    }
+  };
+
   return (
-    <div>
-      <input
-        type="text"
-        value={filtering}
-        onChange={(e) => setFiltering(e.target.value)}
-      />
+    <div className="m-2 border-solid  border-2 border-customColors-gray500 rounded-lg">
+      <div className="flex m-2">
+        <input
+          type="text"
+          value={filtering}
+          placeholder="Search countries..."
+          onChange={(e) => setFiltering(e.target.value)}
+          className="border-solid w-72 border-2 border-customColors-gray500 placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-customColors-blue500"
+        />
+      </div>
+
       <table>
         <tbody>
           {getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
-                <th key={header.id}>
+                <th
+                  key={header.id}
+                  className="w-72 border-solid border-customColors-gray500 border-b-2 uppercase p-4 text-sm">
                   {header.isPlaceholder
                     ? null
                     : flexRender(
@@ -50,10 +71,29 @@ const List = ({ data, columns, setRowSelection }: ListProps) => {
         </tbody>
         <tbody>
           {getRowModel().rows.map((row) => (
-            <tr key={row.id}>
+            <tr
+              key={row.id}
+              className={
+                row.id === selectedRowId ? "bg-customColors-green200" : ""
+              }>
               {row.getVisibleCells().map((cell) => (
-                <th key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                <th key={cell.id} className="h-20  flex-col">
+                  <p className="mt-5 text-sm">
+                    {cell.column.id === "name" && (
+                      <input
+                        className="-ml-8"
+                        type="checkbox"
+                        checked={cell.row.original.name === selectedCountry}
+                        onChange={() =>
+                          handleCheckboxChange(
+                            cell.row.original.name,
+                            cell.row.id
+                          )
+                        }
+                      />
+                    )}
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </p>
                 </th>
               ))}
             </tr>
