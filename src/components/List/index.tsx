46f4f5c9580a27cd6e19ddc89ba67db7ea741c68
Umut Tosun <umut35137@gmail.com>
2023-09-11 23:18:17 +0300

    Feat: filtering

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,26 +1,36 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  getFilteredRowModel,
 } from "@tanstack/react-table";
 import { ListProps } from "./types";
 
-const List = ({ data, columns, rowSelection, setRowSelection }: ListProps) => {
+const List = ({ data, columns, setRowSelection }: ListProps) => {
   const memoizedData = useMemo(() => data, [data]);
   const memoizedColumns = useMemo(() => columns, [columns]);
+
+  const [filtering, setFiltering] = useState<any>();
 
   const { getHeaderGroups, getRowModel } = useReactTable({
     data: memoizedData,
     columns: memoizedColumns,
-    state: {
-      rowSelection,
-    },
     onRowSelectionChange: setRowSelection,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      globalFilter: filtering,
+    },
+    onGlobalFilterChange: setFiltering,
   });
   return (
     <div>
+      <input
+        type="text"
+        value={filtering}
+        onChange={(e) => setFiltering(e.target.value)}
+      />
       <table>
         <tbody>
           {getHeaderGroups().map((headerGroup) => (
