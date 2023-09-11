1dd5ee890e9139749417e9ea42fd84bf09f5d940
Umut Tosun <umut35137@gmail.com>
2023-09-11 22:15:08 +0300

    Feat: react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Home } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
